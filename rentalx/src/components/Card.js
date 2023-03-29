051332147eaa2647cd46d10e729909fe96c47713
Venawm <pandesamir3@gmail.com>
2023-03-29 21:42:23 +0545

    Added some Stying and functionality to modal

--- rentalx/src/components/Card.js
+++ rentalx/src/components/Card.js
@@ -2,12 +2,8 @@ import React from 'react'
 import 'styled-components';
 import styled from 'styled-components';
 import img from '../assests/img.jpg'
-import ReactStars from 'react-stars'
-const Card=({setIsOpen})=> {
-console.log(setIsOpen)
-  const price = 100000;
-  const year = 2008;
-  const carName = "Ferrari Enzo";
+// import ReactStars from 'react-stars'
+const Card=({setIsOpen,year,price,carName})=> {
   const handleOpenModal = () => {
     setIsOpen(true);
   };
@@ -16,66 +12,73 @@ console.log(setIsOpen)
     <Main>
         <img src = {img} alt="" />
         <div className="text">
-          <span className="price">Rs {price} <span className="days">/Days</span></span>
           <span className="price">{carName} <span className="days">{year}</span></span>
-          <ReactStars className='stars' size={30} edit={false} value={3} ></ReactStars>
-          <button onClick={handleOpenModal}>View more</button>
+          <span className="price">Rs {price} <span className="days">/Days</span></span>
+          
+          {/* <ReactStars className='stars' size={30} edit={false} value={3} ></ReactStars> */}
+          <button onClick={handleOpenModal}>View Details</button>
         </div>
 
     </Main>
   )
 }
 const Main = styled.div`
-  margin: 0 0.5rem 0 0.5rem;
-  margin-top: 2rem;
-    padding: 0;
+margin: 0px 15px 0px 15px;
+  margin-top: 5rem;
+  padding: 10px;
   background-color: #F5F5F5;
-  width: 18vw;
-  height: 52vh;
+  width: 20rem;
+  height: 60vh;
   border-radius: 15px;
   transition: transform .2s;
-  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  box-shadow: rgba(149, 157, 165, 0.5) 0px 8px 24px;
   :hover{
-    transform: scale(1.1);
+    transform: scale(1.03);
   }
   img{
     width: 100%;
     height: 30vh;
     object-fit: cover;
-    border-radius: 15px;
+    border-radius: 15px 15px 0px 0px;
   }
   .text{
+    border-radius: 0px 0px 15px 15px;
+    font-size: x-large;
+    margin-top: 5px;
     display: flex;
+    background-color: #F5F5F5;
     flex-direction: column;
     text-align: left;
+    padding: 10px;
     span{
       background-color: #F5F5F5;
-      font-size: larger;
+      font-size: 1.4rem;
       font-weight: 700;
       .days{
         font-size:medium;
         color: #FF5722;
       }
     }
-    .stars{
-      background-color: #F5F5F5;
-    }
+  
     button{
-        border: none;
-        background-color: #FF5722;
-        height: 3rem;
-        width: 100%;
-        font-size: medium;
-        font-weight: 700;
-        color: #EEEEEE;
-       
-        border-radius: 10px;
+      border: none;
+    background-color: #FF5722;
+    height: 70px;
+    width: 100%;
+    font-size: x-large;
+    font-weight: 700;
+    color: #EEEEEE;
+    margin-top: 35px;
+    border-radius: 15px;
+    /* border-radius: 0px 0px 5px 5px; */
+    border-radius: 15px;
         :hover{
             cursor: pointer;
             background-color: #ffb39b;          
         }
     }
   }
+  
 `;
 
 export default Card
--- rentalx/src/pages/CarsList.js
+++ rentalx/src/pages/CarsList.js
@@ -4,9 +4,172 @@ import { useTransition, animated, config } from 'react-spring';
 import NavBar from '../components/NavBar';
 import Card from '../components/Card';
 import { RxCrossCircled } from 'react-icons/rx';
-
+import img from '../assests/ferrari.png'
 const CarsList = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const carsData = [
+    {
+      "price": 100000,
+      "year": 2008,
+      "carName": "Ferrari Enzo",
+      "engine":"v12",
+      "topSpeed":"280Mph",
+      "milage":"5Mpl"
+    },
+    {
+      "price": 50000,
+      "year": 2015,
+      "carName": "Audi R8",
+      "engine":"v8",
+      "topSpeed":"205Mph",
+      "milage":"10Mpl"
+    },
+    {
+      "price": 250000,
+      "year": 2022,
+      "carName": "Bugatti Chiron",
+      "engine":"w16",
+      "topSpeed":"261Mph",
+      "milage":"2Mpl"
+    },
+    {
+      "price": 75000,
+      "year": 2019,
+      "carName": "Tesla Model S",
+      "engine":"electric",
+      "topSpeed":"163Mph",
+      "milage":"350Mpl"
+    },
+    {
+      "price": 30000,
+      "year": 2020,
+      "carName": "Toyota Camry",
+      "engine":"v6",
+      "topSpeed":"130Mph",
+      "milage":"25Mpl"
+    },
+    {
+      "price": 40000,
+      "year": 2017,
+      "carName": "Honda Accord",
+      "engine":"inline-4",
+      "topSpeed":"120Mph",
+      "milage":"30Mpl"
+    },
+    {
+      "price": 200000,
+      "year": 2021,
+      "carName": "Porsche 911",
+      "engine":"flat-6",
+      "topSpeed":"191Mph",
+      "milage":"18Mpl"
+    },
+    {
+      "price": 150000,
+      "year": 2022,
+      "carName": "Lamborghini Huracan",
+      "engine":"v10",
+      "topSpeed":"201Mph",
+      "milage":"8Mpl"
+    },
+    {
+      "price": 35000,
+      "year": 2019,
+      "carName": "Ford Mustang",
+      "engine":"v8",
+      "topSpeed":"155Mph",
+      "milage":"20Mpl"
+    },
+    {
+      "price": 45000,
+      "year": 2020,
+      "carName": "Chevrolet Camaro",
+      "engine":"v8",
+      "topSpeed":"165Mph",
+      "milage":"18Mpl"
+    },{
+      "price": 100000,
+      "year": 2008,
+      "carName": "Ferrari Enzo",
+      "engine":"v12",
+      "topSpeed":"280Mph",
+      "milage":"5Mpl"
+    },
+    {
+      "price": 50000,
+      "year": 2015,
+      "carName": "Audi R8",
+      "engine":"v8",
+      "topSpeed":"205Mph",
+      "milage":"10Mpl"
+    },
+    {
+      "price": 250000,
+      "year": 2022,
+      "carName": "Bugatti Chiron",
+      "engine":"w16",
+      "topSpeed":"261Mph",
+      "milage":"2Mpl"
+    },
+    {
+      "price": 75000,
+      "year": 2019,
+      "carName": "Tesla Model S",
+      "engine":"electric",
+      "topSpeed":"163Mph",
+      "milage":"350Mpl"
+    },
+    {
+      "price": 30000,
+      "year": 2020,
+      "carName": "Toyota Camry",
+      "engine":"v6",
+      "topSpeed":"130Mph",
+      "milage":"25Mpl"
+    },
+    {
+      "price": 40000,
+      "year": 2017,
+      "carName": "Honda Accord",
+      "engine":"inline-4",
+      "topSpeed":"120Mph",
+      "milage":"30Mpl"
+    },
+    {
+      "price": 200000,
+      "year": 2021,
+      "carName": "Porsche 911",
+      "engine":"flat-6",
+      "topSpeed":"191Mph",
+      "milage":"18Mpl"
+    },
+    {
+      "price": 150000,
+      "year": 2022,
+      "carName": "Lamborghini Huracan",
+      "engine":"v10",
+      "topSpeed":"201Mph",
+      "milage":"8Mpl"
+    },
+    {
+      "price": 35000,
+      "year": 2019,
+      "carName": "Ford Mustang",
+      "engine":"v8",
+      "topSpeed":"155Mph",
+      "milage":"20Mpl"
+    },
+    {
+      "price": 45000,
+      "year": 2020,
+      "carName": "Chevrolet Camaro",
+      "engine":"v8",
+      "topSpeed":"165Mph",
+      "milage":"18Mpl"
+    }
+  ];
+  
+  
 
   const handleCloseModal = () => {
     setIsOpen(false);
@@ -24,16 +187,25 @@ const CarsList = () => {
       <NavBar />
       <Main>
         <p className="heading">Our Vehicle Collections</p>
+        <div class="searchbar"></div>
         <div className="cards">
-          <Card setIsOpen={setIsOpen} />
+          {carsData.map((carData)=>{
+              return <Card price={carData.price} year={carData.year} carName={carData.carName} setIsOpen={setIsOpen} />
+          })}
+          
         </div>
       </Main>
       {modalTransition((style, item) =>
         item ? (
-          <ModalBackdrop onClick={handleCloseModal}>
+          <ModalBackdrop>
             <Modal style={style}>
               <RxCrossCircled className="button" onClick={handleCloseModal} />
-              <p>Modal Content Here</p>
+              <div class="modalContent">
+                <h1>Enzo</h1>
+                <img src={img} alt=""></img>
+              </div>
+              <div class="details"></div>
+              
             </Modal>
           </ModalBackdrop>
         ) : null
@@ -71,7 +243,7 @@ const Main = styled.div`
     margin-top: 1rem;
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-between;
+    justify-content: space-evenly;
   }
 `;
 
@@ -87,16 +259,12 @@ const Modal = styled(animated.div)`
   z-index: 2;
   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
   padding: 1.5rem;
-  display: flex;
+  /* display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: center; */
 
-  p {
-    font-size: 2rem;
-    font-weight: 700;
-    margin-bottom: 2rem;
-  }
+ 
 
   .button {
     position: absolute;
@@ -105,6 +273,23 @@ const Modal = styled(animated.div)`
     font-size: 2rem;
     color: #ff5722;
     cursor: pointer;
+    background-color: #f5f5f5;
+  }
+  .modalContent{
+    margin-top: 3vw;
+    background-color: #f5f5f5;
+    text-align: center;
+    h1{
+    background-color: #f5f5f5;
+
+    }
+    img{
+      
+      background-color: transparent;
+      width: 45vw;
+      margin-top: -5rem;
+      
+    }
   }
 `;
 
